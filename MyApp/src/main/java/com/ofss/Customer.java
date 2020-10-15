9a15f80360d02b37fae91909c29d98dbfd1f92da
Eldho <eldhoshibu123@gmail.com>
2020-10-15 14:59:27 +0530

    New Customer Added

---
+++ MyApp/src/main/java/com/ofss/Customer.java
@@ -0,0 +1,33 @@
+package com.ofss;
+
+public class Customer {
+	private String name;
+	private String location;
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", location=" + location + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public Customer(String name, String location) {
+		super();
+		this.name = name;
+		this.location = location;
+	}
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
--- MyApp/src/main/java/com/ofss/HelloWorld.java
+++ MyApp/src/main/java/com/ofss/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 			System.out.println("My first program in git from eclipse");
-			
+			Customer c=new Customer("eldho","kochi");
+			System.out.println(c);
 	}
 
 }
